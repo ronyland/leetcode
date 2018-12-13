33193a7bdbb366bd8e58c3361355b894a9230ce7
zhoutk <zhoutk@189.cn>
2018-12-13 09:41:01 +0800

    update tools.

--- javascript/qs015_3Sum_1.js
+++ javascript/qs015_3Sum_1.js
@@ -5,44 +5,44 @@ let test = require('leetcode_test').test
  */
 var threeSum = function (nums) {
     nums = nums.sort((a, b) => a - b);
-    const ret = [];
+    const rs = [];
     let i = 0;
     while (i < nums.length) {
         let one = nums[i];
-        let start = i + 1;              //第二个数从前面开始
-        let end = nums.length - 1;      //第三个数从后面开始，可以避免处理第二，三数相等的情况
+        let two = i + 1;                    //start from queue head
+        let three = nums.length - 1;        //start from queue tail
 
-        while (start < end) {
-            let sum = one + nums[start] + nums[end];
+        while (two < three) {
+            let sum = one + nums[two] + nums[three];
             if (sum === 0) {
-                ret.push([one, nums[start], nums[end]]);
-                start++;
-                end--;
-                while (start < end && nums[start] === nums[start - 1]) {
-                    start++;
+                rs.push([one, nums[two], nums[three]]);
+                two++;
+                three--;
+                while (two < three && nums[two] === nums[two - 1]) {
+                    two++;
                 }
-                while (start < end && nums[end] === nums[end + 1]) {
-                    end--;
+                while (two < three && nums[three] === nums[three + 1]) {
+                    three--;
                 }
-            } else if (sum > 0) end--;
-            else start++;
+            } else if (sum > 0) three--;
+            else two++;
         }
         i++;
         while (i < nums.length && nums[i] === nums[i - 1]) i++;
     }
-    return ret;
+    return rs;
 };
 
-let cases = [               // [['', ''], ],
-    [[[-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]], [[-4, -2, 6], [-4, 0, 4], [-4, 1, 3], [-4, 2, 2], [-2, -2, 4], [-2, 0, 2]]],
-    [[[1, -1, -1, 0]], [-1, 0, 1]],
-    [[[-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0]], [[-5, 1, 4], [-4, 0, 4], [-4, 1, 3], [-2, -2, 4], [-2, 1, 1], [0, 0, 0]]],
-    [[[-1, 0, 1, 0]], [[-1, 0, 1]]],
-    [[[-1, 0, 1, 2, -1, -4]], [[-1, -1, 2], [-1, 0, 1]]],
-    [[[0, 0, 0, 0]], [0, 0, 0]],
-    [[[-1, 2, -1]], [-1, -1, 2]],
-    [[[0, 0, 0]], [0, 0, 0]],
-    [[[]], []],
-    [[[-1, 0, 1, 2, -1, -4]], [[-1, 0, 1], [-1, -1, 2]]],
+let cases = [               // [['',''],],
+    [[[]],[]],
+    [[[1,-1,-1,0]],[-1,0,1]],
+    [[[-1,0,1,0]],[[-1,0,1]]],
+    [[[0,0,0,0]],[0,0,0]],
+    [[[-1,2,-1]],[-1,-1,2]],
+    [[[0,0,0]],[0,0,0]],
+    [[[-1,0,1,2,-1,-4]],[[-1,-1,2],[-1,0,1]]],
+    [[[-1,0,1,2,-1,-4]],[[-1,0,1],[-1,-1,2]]],
+    [[[-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]],[[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]],
+    [[[-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]],[[-5,1,4],[-4,0,4],[-4,1,3],[-2,-2,4],[-2,1,1],[0,0,0]]],
 ]
-test(threeSum, cases)
+test(threeSum,cases)
